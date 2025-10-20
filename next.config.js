999910298d4df29563bb72e99affe6f71aba320a
Fred Malack <fredmalack15@gmail.com>
2025-10-21 00:16:39 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Fix chunk loading issues
   experimental: {
     optimizePackageImports: [
@@ -43,4 +42,4 @@ const nextConfig: NextConfig = {
   output: undefined
 };
 
-export default nextConfig;
+module.exports = nextConfig;
